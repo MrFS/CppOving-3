12aa18a1dd95d5bed862cc7752f9399ab5abdf12
Joachim Flatberg Stamnes <joachifs@student.hist.no>
2016-02-17 20:34:28 +0100

    Oppg1

--- Oving3/Oving3/main.cpp
+++ Oving3/Oving3/main.cpp
@@ -2,35 +2,72 @@
 
 using namespace std;
 
-double lesFlyttTall(int a,int b);
-
-int main() {
+	double veksle(int a, int b);
+	char YN;
 	int inn;
 	int kurs;
-	//double tall = lesFlyttTall(kurs, inn);
+
+int main() {
 
 	cout << "Skriv inn kurs:" << endl;
+
 	cin >> kurs;
 
-	cout << "Hvor mye skal du veksle?" << endl;
+	cout << "Skal det beregnes FRA Norske kronasjer? J/N" << endl;
 
-	cin >> inn;
+	cin >> YN;
 
-	cout << lesFlyttTall(kurs, inn);
+	YN = tolower(YN);
+	
+	switch (YN)	
+	{
+
+	case 'j':
+
+		cout << "Oppgi ønsket beløp i norske kronasjer:" << endl;
+
+		cin >> inn;
+
+		cout << "Det blir " << veksle(inn, kurs) << " i fremmed valuta." << endl;
+
+		break;
+
+	case 'n':
+
+		cout << "Oppgi ønsket beløp i fremmed valuta:" << endl;
+
+		cin >> inn;
+
+		cout << "Det blir " << veksle(inn, kurs) << " i norske kronasjer." << endl;
+
+		break;
+
+	default:
+		cout << "Kun J/N!";
+		break;
+	}
+
+	cout << endl;
 
 	system("Pause");
 
 	return 0;
 }
 
-double lesFlyttTall(int a, int b) {
+double veksle(int a, int b) {
+	switch (YN)
+	{
 
-	double res;
+	case 'j':
 
-	res = b * 100 / a;
+		return a * 100 / b;
 
+		break;
 
+	case 'n':
 
-	return res;
+		return a * b / 100;
 
+		break;
+	}
 }
